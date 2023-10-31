671ae8026b17ef80247330cc1ab672fc250febb6
lycoris2023 <135022049+lycoris2023@users.noreply.github.com>
2023-10-31 20:06:20 +0800

    Create add.java

---
+++ add.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class add{
+    public static void main(String[]args){       
+        Scanner input=new Scanner(System.in);
+        System.out.print("enter the start: ");
+        int start= input.nextInt();
+        System.out.print("enter the end: ");
+        int end=input.nextInt();
+        if(start<end){
+            int sum=0;
+            for(int counter=start;counter<=end;counter++){
+                sum+=counter;
+            }
+            System.out.println("The sum is "+sum);   
+        }
+        else{
+            System.out.println("Error")
+        }
+    }
+}
